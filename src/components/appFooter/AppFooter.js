92940cd86b8a1dda57180b236b085c0b982e1e9f
BijjamVijayReddy <bijjamvijayreddy@gmail.com>
2024-09-12 13:18:11 +0530

    Folder is Created

---
+++ src/components/appFooter/AppFooter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AppFooter = () => {
+  return (
+    <div>
+       <h2>AppFooter</h2>
+    </div>
+  )
+}
+
+export default AppFooter
---
+++ src/components/appHeader/AppHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AppHeader = () => {
+    return (
+        <div>
+            <h2>AppHeader</h2>
+        </div>
+    )
+}
+
+export default AppHeader
