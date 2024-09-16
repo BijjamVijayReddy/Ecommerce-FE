053782782ec4a9428b6bbb03e5718716b4f1c5e2
chaitanyakumar99 <gchkumar999@gmail.com>
2024-09-16 12:26:21 +0530

    latest one

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -66,7 +66,7 @@ const Login = () => {
                     </span>
                     <br /><br />
                 </div>
-                <p className='signupnow'>Don't have an account ? <a className='signup' href="#">Sign up now ?</a></p>SIGN UP NOW?!
+                <p className='signupnow'>Don't have an account ? <a className='signup' href="#">Sign up now ?</a></p>
                 <br />
                 <button type="submit" className="login-button">Login</button>
             </form>
