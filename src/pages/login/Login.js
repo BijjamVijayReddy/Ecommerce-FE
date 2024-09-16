b1e18fc41e76db9481878f16f1b7716872876916
BijjamVijayReddy <bijjamvijayreddy@gmail.com>
2024-09-16 13:48:25 +0530

    Font cahnges are Upadtes

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -9,7 +9,7 @@ const eyeSvg = (<svg xmlns="http://www.w3.org/2000/svg" fill="none" style={{ col
 );
 
 const eyeClose = (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none"  style={{ color: "brown" }} viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" style={{ color: "brown" }} viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
     </svg>
 
@@ -36,12 +36,12 @@ const Login = () => {
     return (
         <div className='login-container'>
             <img src={logo} alt="logo" className='logo' />
-            <h1 className='Login-shift'>Log in to shift cart</h1>
-            <p className='Login-shift1'>Welcome back please log in to continue</p>
+            <h1 className='Login-shift'>Log in to Shift Cart</h1>
+            <p className='Login-shift1'>Welcome Back Please Log-in to Continue</p>
             <br />
             <form onSubmit={handleSubmit} className="login-form">
                 <div className="input-group">
-                    <label>Email Address/Mobile Number</label>
+                    <label>Email Address</label>
                     <input className='input'
                         type="email"
                         id="email"
@@ -66,7 +66,7 @@ const Login = () => {
                     </span>
                     <br /><br />
                 </div>
-                <p className='signupnow'>Don't have an account ? <a className='signup' href="#">Sign up now ?</a></p>
+                <p className='signupnow'>Don't have an Account ? <a className='signup' href="#">Sign up now ?</a></p>
                 <br />
                 <button type="submit" className="login-button">Login</button>
             </form>
