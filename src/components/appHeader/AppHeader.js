d8133038304e5ca72ebd1c9a5d17a627896d7568
BijjamVijayReddy <bijjamvijayreddy@gmail.com>
2024-09-23 19:27:56 +0530

    changes are Done

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -10,7 +10,7 @@ import React, { useState, useEffect } from 'react';
 
 
 
-const Header = () => {
+const AppHeader = () => {
   const navigate = useNavigate()
   const [isScrolled, setIsScrolled] = useState(false);
   const [showProfileDropdown, setShowProfileDropdown] = useState(false);
@@ -18,7 +18,7 @@ const Header = () => {
   // Add scroll event listener
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 50) { 
+      if (window.scrollY > 50) {
         setIsScrolled(true);
       } else {
         setIsScrolled(false);
@@ -34,42 +34,42 @@ const Header = () => {
   }, []);
   const toggleProfileDropdown = () => {
     setShowProfileDropdown(!showProfileDropdown);
-    
+
   };
 
   return (
-    <header  className={`header ${isScrolled ? 'scrolled' : ''}`}>
+    <header className={`header ${isScrolled ? 'scrolled' : ''}`}>
       <div className="container">
         <div className="logo">
-        <img className='logo2'src={logo} alt="Logo" />
-          </div>
+          <img className='logo2' src={logo} alt="Logo" />
+        </div>
 
         <input type="checkbox" id="menu-toggle" className="menu-toggle" />
         <nav className="nav">
           <ul>
-          <li><NavLink to="/dashboard" className={({ isActive }) => isActive ? " text-[brown] font-bold" : null}>Home</NavLink></li>
-            <li><NavLink to="/categories"ClassName="active-link">Categories</NavLink></li>
-            <li><NavLink to="/about"ClassName={({ isActive }) => (isActive ? " brown active-link" : "brown")}>About Us</NavLink></li>
+            <li><NavLink to="/dashboard" className={({ isActive }) => isActive ? " text-[brown] font-bold" : null}>Home</NavLink></li>
+            <li><NavLink to="/categories" ClassName="active-link">Categories</NavLink></li>
+            <li><NavLink to="/about" ClassName={({ isActive }) => (isActive ? " brown active-link" : "brown")}>About Us</NavLink></li>
             <li><NavLink to="/contact" ClassName="active-link">Contact Us</NavLink></li>
           </ul>
         </nav>
 
-        <div className="cart-login">  
-        < div className="shopping-cart-icon">
-          <FontAwesomeIcon icon={faShoppingCart} />
-          <span className="cart-count">0</span>
-        </div>
-        <div className="profile-icon" onClick={toggleProfileDropdown}>
-          <FontAwesomeIcon icon={faUserCircle} size="2x"/>
-         </div>
-         {showProfileDropdown && (
+        <div className="cart-login">
+          < div className="shopping-cart-icon">
+            <FontAwesomeIcon icon={faShoppingCart} />
+            <span className="cart-count">0</span>
+          </div>
+          <div className="profile-icon" onClick={toggleProfileDropdown}>
+            <FontAwesomeIcon icon={faUserCircle} size="2x" />
+          </div>
+          {showProfileDropdown && (
             <div className="profile-dropdown">
               <ul>
                 <li onClick={() => navigate('/MyProfile')}>MyProfile</li>
                 <li onClick={() => navigate('/Payment History')}>Payment History</li>
                 <li onClick={() => navigate('/settings')}>Settings</li>
-                
-                <li className="browne" onClick={() => navigate('/login')} style={{color:'white'}}>Login</li>
+
+                <li className="browne" onClick={() => navigate('/login')} style={{ color: 'white' }}>Login</li>
               </ul>
             </div>
           )}
@@ -83,4 +83,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default AppHeader;
