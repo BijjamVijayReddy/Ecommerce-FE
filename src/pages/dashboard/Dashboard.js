eab37120a29d685fa98bcd5524541430fccf27d9
BijjamVijayReddy <bijjamvijayreddy@gmail.com>
2024-09-23 18:27:16 +0530

    package. lockson is Upadted

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import AppHeader from '../../components/appHeader/AppHeader'
 import AppFooter from '../../components/appFooter/AppFooter'
 
 const Dashboard = () => {
     return (
         <div>
-            {/* <AppHeader />
-                <p> This Page is still under construction</p> */}
             <AppFooter />
         </div>
     )
