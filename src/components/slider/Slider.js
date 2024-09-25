22ae3bcf68f4c6ede77b794f785de074e7a64f8e
chaitanyakumar99 <gchkumar999@gmail.com>
2024-09-25 17:18:44 +0530

    slider1

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -5,6 +5,30 @@ import img26 from '../../assests/Img26.webp'
 import img24 from '../../assests/Img24.webp'
 import img12 from '../../assests/img12.jpeg'
 import img13 from '../../assests/Img13.jpg'
+
+
+// Custom Previous Arrow
+const PrevArrow = ({ className, style, onClick }) => (
+  <button
+    className={`${className} prev-arrow`} // Add custom class for styling
+    style={{ ...style, display: "block", position: "absolute", top: "50%", left: "20px", zIndex: 1 }} // Position inside slide
+    onClick={onClick}
+  >
+    &#9664; {/* Unicode for left arrow */}
+  </button>
+);
+
+
+// Custom Next Arrow
+const NextArrow = ({ className, style, onClick }) => (
+  <button
+    className={`${className} next-arrow`} // Add custom class for styling
+    style={{ ...style, display: "block", position: "absolute", top: "50%", right: "20px", zIndex: 1 }} // Position inside slide
+    onClick={onClick}
+  >
+    &#9654; {/* Unicode for right arrow */}
+  </button>
+);
  
 
 const user_img = [
@@ -24,6 +48,8 @@ const Slider = () => {
     autoplay: true,
     autoplaySpeed: 3000,
     swipeToSlide: true,
+    prevArrow:<PrevArrow/>,
+    nextArrow:<NextArrow/>
   };
 
   return (
