2251954ea95bdb1647020e669eaeb4d415327c6b
AVULAVIJJI <20kb1a1205@nbkrist.org>
2024-10-01 12:25:46 +0530

    Internet connection

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import { useEffect, useState } from "react";
 import AppRoutes from "../src/appRoutes/AppRoutes";
 import { useNavigate } from 'react-router-dom';
 import sessionService from "./services/sessionServices";
+import Internet from "../src/utlity/InternetTracker/Internet";
+
 
 
 function App() {
   const navigate = useNavigate();
   const [initialized, setInitialized] = useState(false);
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+  useEffect(() => {
+    const handleOffline = () => {
+      setIsOnline(false);
+    };
+
+    const handleOnline = () => {
+      setIsOnline(true); 
+    };
+
+  
+    window.addEventListener('offline', handleOffline);
+    window.addEventListener('online', handleOnline);
+
+    return () => {
+      window.removeEventListener('offline', handleOffline);
+      window.removeEventListener('online', handleOnline);
+    };
+  }, []);
 
   useEffect(() => {
     if (!initialized) {
@@ -22,7 +43,10 @@ function App() {
 
   return (
     <div>
-      <AppRoutes />
+      {/* <AppRoutes /> */}
+      {/* <Internet /> */}
+      
+       {!isOnline ? <Internet /> : <AppRoutes />}
     </div>
   );
 }
